178a321ee99bfeac6165fcbc58bb0808ff5c4596
maxkim77 <141907655+maxkim77@users.noreply.github.com>
2023-11-28 20:32:00 +0900

    Update login.js

--- js/login.js
+++ js/login.js
@@ -16,31 +16,7 @@ document.getElementById('loginForm').addEventListener('submit', function (event)
           password: password
       })
   })
-//   .then(response => {
-//       if (response.status === 200) {
-//           // Successful login, store the token in local storage
-//           response.json().then(data => {
-//             console.log("Received data:", data); // 서버 응답 로깅
-//             localStorage.setItem('token', data.token);
-//             console.log("Stored token:", localStorage.getItem('token')); // 저장된 토큰 확인
-//             // 토큰 저장을 확인한 후 페이지 전환
-//             window.location.href = 'http://127.0.0.1:5500/FE/index.html';
-//           });
-//       } else if (response.status === 400) {
-//           // Bad request, show error message from the server
-//           response.json().then(data => {
-//               alert(data.error);
-//           });
-//       } else if (response.status === 401) {
-//           // Unauthorized, show error message
-//           alert('Invalid credentials. Please try again.');
-//       } else {
-//           // Handle other error codes here if needed
-//           alert('An error occurred. Please try again later.');
-//       }
-//   })
-//   .catch(error => console.error('Error:', error));
-// });
+
 .then(response => {
     if (response.status === 200) {
         // Successful login
@@ -53,12 +29,12 @@ document.getElementById('loginForm').addEventListener('submit', function (event)
     console.log("Received data:", data); // 서버 응답 로깅
     localStorage.setItem('token', data.token);
     console.log("Stored token:", localStorage.getItem('token')); // 저장된 토큰 확인
-    // 페이지 전환은 여기에서 수행합니다.
-    window.location.href = 'http://127.0.0.1:5500/FE/index.html';
+    // 페이지 전환
+    window.location.href = 'https://maxkim77.github.io/DRFChatService_FE/Repo/service/index.html';
 })
 .catch(error => {
     console.error('Error:', error);
-    // 오류가 발생한 경우 사용자에게 알립니다.
+    // 오류
     alert('Login failed. Please try again.');
 });
 });
